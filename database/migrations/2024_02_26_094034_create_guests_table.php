e62ee50935de623a28eb10acd394351da5e9376d
David Abraham <daciabra@gmail.com>
2024-02-26 11:17:08 +0100

    guests table created

--- database/migrations/2024_02_26_094034_create_guests_table.php
+++ database/migrations/2024_02_26_094034_create_guests_table.php
@@ -13,6 +13,11 @@ return new class extends Migration
     {
         Schema::create('guests', function (Blueprint $table) {
             $table->id();
+            $table->string("g_username", 100)->unique();
+            $table->string("g_password", 100);
+            $table->string("g_name", 100);
+            $table->string("g_email", 200)->unique();
+            $table->string("g_phone_number", 20);
             $table->timestamps();
         });
     }
