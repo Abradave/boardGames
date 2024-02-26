2f4fcd8bb628099d00cb4ae5ddd3429396c07742
David Abraham <daciabra@gmail.com>
2024-02-26 15:34:11 +0100

    BoardGame data listing done

--- app/Http/Controllers/API/BoardGameController.php
+++ app/Http/Controllers/API/BoardGameController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\BoardGame;
 use Illuminate\Http\Request;
 
 class BoardGameController extends Controller
@@ -12,7 +13,8 @@ class BoardGameController extends Controller
      */
     public function index()
     {
-        //
+        $board_games = BoardGame::all();
+        return response()->json($board_games, 200);
     }
 
     /**
