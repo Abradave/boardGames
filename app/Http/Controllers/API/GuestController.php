d6fc965d0bf39d941ff3dd10bc63d3d440ab0444
David Abraham <daciabra@gmail.com>
2024-02-26 12:15:35 +0100

    Data listing done

--- app/Http/Controllers/API/GuestController.php
+++ app/Http/Controllers/API/GuestController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Guest;
 use Illuminate\Http\Request;
 
 class GuestController extends Controller
@@ -12,7 +13,8 @@ class GuestController extends Controller
      */
     public function index()
     {
-        //
+        $guests = Guest::all();
+        return response()->json($guests, 200);
     }
 
     /**
