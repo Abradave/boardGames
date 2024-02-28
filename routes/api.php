724fee147a40e4e8126c5d68aeaf0bafea04c5aa
Bartha Ariana <ariabartha@gmail.com>
2024-02-28 20:33:01 +0100

    EmployeeController added to the previously defined route

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\API\BoardGameController;
+use App\Http\Controllers\API\EmployeeController;
 use App\Http\Controllers\API\GuestController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -24,4 +25,4 @@ Route::apiResource("/guest", GuestController::class);
 
 Route::apiResource("/boardgame", BoardGameController::class);
 
-Route::apiResource("/employee", null);
+Route::apiResource("/employee", EmployeeController::class);
