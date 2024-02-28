31be139fda0247a5f907ed2fdb952180cdd2e777
Bartha Ariana <ariabartha@gmail.com>
2024-02-28 20:37:51 +0100

    Employee data listing done

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Employee;
 use Illuminate\Http\Request;
 
 class EmployeeController extends Controller
@@ -12,7 +13,8 @@ class EmployeeController extends Controller
      */
     public function index()
     {
-        //
+        $employees = Employee::all();
+        return response()->json($employees, 200);
     }
 
     /**
