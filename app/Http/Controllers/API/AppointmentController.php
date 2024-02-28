591daacd19186d6cd896afaf874a8d63f0fc24ea
Bartha Ariana <ariabartha@gmail.com>
2024-02-28 23:17:19 +0100

    data listing done for Appointments

--- app/Http/Controllers/API/AppointmentController.php
+++ app/Http/Controllers/API/AppointmentController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
+use App\Models\Appointment;
 use Illuminate\Http\Request;
 
 class AppointmentController extends Controller
@@ -12,7 +13,8 @@ class AppointmentController extends Controller
      */
     public function index()
     {
-        //
+        $appointments = Appointment::all();
+        return response()->json($appointments, 200);
     }
 
     /**
