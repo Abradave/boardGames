7885c7db7098ed003896ed0a17bfa1f8e83c264d
Bartha Ariana <ariabartha@gmail.com>
2024-04-12 16:13:08 +0200

    employee login created

--- app/Http/Controllers/API/EmployeeController.php
+++ app/Http/Controllers/API/EmployeeController.php
@@ -36,13 +36,20 @@ class EmployeeController extends Controller
             "e_email" =>$request->e_email,
             "e_password" => Hash::make($request->e_password)
         ]);
-        return $employee;
+        return response()->json($employee, 201);
 
     }
 
     public function employeeLogin(LoginEmployeeRequest $request)
     {
-        
+        $employee = Employee::where("e_email", $request->e_email)->first();
+
+        if(!$employee || !Hash::check($request->e_password, $employee->e_password)){
+            return response()->json(["message" => "Incorrect email or password."], 401);
+        }
+
+        $e_token = $employee->createToken("EmployeeAuthToken")->plainTextToken;
+        return response()->json(["token" => $e_token]);
     }
 
     public function employeeLogout(Request $request)
